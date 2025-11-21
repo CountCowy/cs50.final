73fe6fec3aea8e043028c068604efa0cb8ff999b
CountCowy <countcowy@dhcp-10-250-238-197.harvard.edu>
2025-11-20 23:12:04 -0500

    Simple index page

--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from scanner import takeit
 from JePeux import query_openai
+import os
+
 
 
 #plan
 #get input n shi working, basic home page with form, jinja blah
+# - separate feet in the algorythm, count how many entires of long'd or shorted vowels have appeared, then put a || at the end of that word or whatever
 #do it with gpt
 #flask index
+#- history page, instruction on how to scan, support/contact page with credits
 #registering and log in feature
 #database to store previous inputs, user info etc.
 #admin panel?
 
 
-
-app = Flask(__name__)
+custom_template_path = os.path.join(os.path.dirname(__file__), 'pantheon')
+app = Flask(__name__, template_folder=custom_template_path)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
 
 
 @app.after_request
@@ -28,3 +33,17 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+
+@app.route("/")
+def index():
+    """Show portfolio of stocks and add money to account"""
+    if request.method == "POST":
+        #get scansion.
+        #redirect to scansion page, with template filled with scansion stuff
+        pass
+    
+
+    return render_template("index.html",)
+
+
