e906ede7c3b4637ed4cc9b711d820781d1829ac6
CountCowy <kingyareh@gmail.com>
2025-11-24 00:23:37 -0500

    vercel adjustment 9

--- app.py
+++ app.py
@@ -28,18 +28,24 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-g.db = sqlite3.connect("widener.db", check_same_thread=False)
-conn = g.db
-conn.row_factory = sqlite3.Row
-mouse = conn.cursor()
-mouse.execute("""
+#database connection
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("widener.db")
+        g.db.row_factory = sqlite3.Row
+    return g.db
+def init_db():
+    global db
+    global mouse
+    db = get_db()
+    db.executescript("""
     CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     username TEXT NOT NULL,
     hash TEXT NOT NULL,
     email TEXT NOT NULL);
     """)#note that we dont have any email confirmation yet
-mouse.execute("""
+    db.executescript("""
     CREATE TABLE IF NOT EXISTS history(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     user_id INTEGER NOT NULL,
@@ -47,11 +53,12 @@ mouse.execute("""
     algorithm_scan TEXT NOT NULL,
     gpt_scan TEXT NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-    );
-    """)#note that we dont have any email confirmation yet
-conn.commit()
-
+    FOREIGN KEY (user_id) REFERENCES users(id));
+    """)
+    mouse = db.cursor()
+    
+with app.app_context():
+    init_db()
 
 @app.after_request
 def after_request(response):
